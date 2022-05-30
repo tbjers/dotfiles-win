6e25a9d1d41b75b3e3c9c505c43cc6bb61848fbc
Torgny Bjers <torgny@bjers.org>
2022-05-30 12:14:55 -0400

    add Visual Studio Build Tools installer helper

---
+++ dot_config/powershell/install-vsbuildtools.ps1
@@ -0,0 +1,1 @@
+.\vs_buildtools.exe --channelURI https://aka.ms/vs/16/release/channel --includeRecommended --add Microsoft.VisualStudio.Workload.AzureBuildTools --add Microsoft.VisualStudio.Workload.MSBuildTools --add Microsoft.VisualStudio.Workload.NodeBuildTools --add Microsoft.VisualStudio.Workload.VCTools --passive
